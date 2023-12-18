f8215c1111985372a76a09b02f0d8c4dbd575ab6
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-18 21:25:55 +0700

    feat: add starting image & starting page

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue.shade300,
+              Colors.blue.shade100,
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: const StartScreen(),
+      ),
     ),
   ));
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,31 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('starting');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Quiz App With Flutter',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.blue.shade900,
+            ),
+            child: const Text('Start'),
+          )
+        ],
+      ),
+    );
   }
 }
